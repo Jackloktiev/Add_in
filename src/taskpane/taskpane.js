a2da2e20fae0ecd13d47cdbac0961312bfa1b64f
Jack Loktiev <jack.loktiev@gmail.comm>
2021-11-18 19:29:36 -0500

    Authentication gets the token. Basic code for session storage

---
+++ src/taskpane/taskpane.js
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+ * See LICENSE in the project root for license information.
+ */
+
+/* global document, Office, require */
+
+const ssoAuthHelper = require("./../helpers/ssoauthhelper");
+
+Office.onReady((info) => {
+  if (info.host === Office.HostType.Excel) {
+    document.getElementById("getGraphDataButton").onclick = ssoAuthHelper.getGraphData;
+    document.getElementById("local-storage-save").onclick = setLocalStorage;
+    document.getElementById("local-storage-get").onclick = getLocalStorage;
+    console.log("set set set")
+  }
+});
+
+function setLocalStorage(){
+  console.log('button1 clicked...')
+  window.sessionStorage.setItem('token', 'this is my saved token')
+}
+
+function getLocalStorage(){
+  console.log('button1 clicked...')
+  var token = window.sessionStorage.getItem('token')
+  console.log(token)
+}
